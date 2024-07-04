89774b3a6837386b958697cff7d7cd9704372573
Daniel Yu <daniel.yu@displayr.com>
2024-07-04 11:47:24 +0930

    test

--- src/infrastructure/src/index.ts
+++ src/infrastructure/src/index.ts
@@ -1,3 +1,6 @@
+import { K8sProvider } from './azure/k8s-provider';
 import { ServiceResourceGroupResource } from './azure/service-resource-group-resource';
 
 new ServiceResourceGroupResource().resourceGroup;
+
+new K8sProvider();
